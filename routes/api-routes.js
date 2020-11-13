b2b3783b670cfa80702b702c16070d6d89b5fb48
Dung Nguyen <dudu.nt107@gmail.com>
2020-11-12 18:44:13 -0600

    added api route to create new workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,6 +15,18 @@ module.exports = (app) => {
     });
   });
 
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, {$push:{exercises:req.body}},
+       {new:true}, (err,data) => {
+           if (err) throw err
+           else {
+               res.json(data)
+           }
+       }
+      )
+});
+
+
  
 };
 
